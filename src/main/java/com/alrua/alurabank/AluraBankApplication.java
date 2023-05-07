e337a3632f1616b4f8198367df627bb33d67a682
Ricardo Morello <ricardo_morello@hotmail.com>
2023-05-07 17:20:58 -0300

    Initial commit

---
+++ src/main/java/com/alrua/alurabank/AluraBankApplication.java
@@ -0,0 +1,13 @@
+package com.alrua.alurabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraBankApplication.class, args);
+	}
+
+}
