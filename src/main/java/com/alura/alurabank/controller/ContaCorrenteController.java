b8a0ca1e7b18bacd6151c5518031dacb0e2b4a9d
Ricardo Morello <ricardo_morello@hotmail.com>
2023-05-07 17:53:41 -0300

    Update consultar saldo

--- src/main/java/com/alura/alurabank/controller/ContaCorrenteController.java
+++ src/main/java/com/alura/alurabank/controller/ContaCorrenteController.java
@@ -1,5 +1,6 @@
 package com.alura.alurabank.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/alura/alurabank/controller/AluraBankApplicationTests.java
+++ src/test/java/com/alura/alurabank/controller/AluraBankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.alura.alurabank;
+package com.alura.alurabank.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
